903d561f26c63813baaa9a066197f191393f254c
Logan McDaniel <lsmcd@pm.me>
2024-06-12 19:11:12 -0600

    fix import bug

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
-const Category = require("../models/Category.js");
-const Order = require("../models/Order.js");
-const Product = require("../models/Product.js");
-const User = require("../models/User.js");
+const Category = require("./Category.js");
+const Order = require("./Order.js");
+const Product = require("./Product.js");
+const User = require("./User.js");
 
 module.exports = { Category, Order, Product, User };
