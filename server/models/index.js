dadb8ee3c0a4de5fa83d6a9ed7595d3f1c00c8f8
MARTIN VIVANCO <martinvivanco@live.com.mx>
2024-06-12 21:09:42 -0600

    NUEVOS CAMBIOS

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
-const Category = require("./Category.js");
-const Order = require("./Order.js");
-const Product = require("./Product.js");
-const User = require("./User.js");
+const Category = require('./Category.js');
+const Order = require('./Order.js');
+const Product = require('./Product.js');
+const User = require('./User.js');
 
 module.exports = { Category, Order, Product, User };
