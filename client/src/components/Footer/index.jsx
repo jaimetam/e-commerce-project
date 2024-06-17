ec725331420d6d9213817de0db570f6253363020
jaimetam <tamesjaime96@gmail.com>
2024-06-17 12:35:01 -0600

    add Footer

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+        MinimalCloth
+    </footer>
+  );
+}
+export default Footer;
--- client/vite.config.js
+++ client/vite.config.js
@@ -19,4 +19,4 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom'
   }
-})
+});
