ae8e5285e3626eab0cf7e4cb4681ffc6a4ac785e
MARTIN VIVANCO <martinvivanco@live.com.mx>
2024-06-16 19:55:31 -0600

    nuevos cambios

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -29,42 +29,61 @@ function Login(props) {
     });
   };
 
+
+
   return (
     <div className="container my-1">
       <Link to="/signup">← Go to Signup</Link>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
+      <h2 className='center'>Log in</h2>
+      <p className='center'>Log into your account to start shopping!</p>
+
+      <div> </div>
+      <div className='center-input'>
+        <form onSubmit={handleFormSubmit}>
           <div>
-            <p className="error-text">The provided credentials are incorrect</p>
+
+            <label htmlFor="email" className='center'>Enter your email address: </label>
+            <br />
+            <input
+              className='center-input'
+              placeholder="example@email.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+
+            />
           </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+
+          <br />
+
+          <div>
+
+            <label htmlFor="pwd" className='center'>Enter your password:</label>
+
+            <input
+              className='center-input'
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+            />
+          </div>
+          <br />
+          {error ? (
+            <div>
+              <p className="error-text">Incorrect credentials</p>
+            </div>
+          ) : null}
+          <div className="center-input">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
+
   );
 }
 
